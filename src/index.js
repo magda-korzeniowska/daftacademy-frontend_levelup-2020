742a1fd9c39a8c0ad0307ef305958c74f2963e1b
magda-korzeniowska <m.korzeniowska@tlen.pl>
2020-04-23 18:53:13 +0200

    feat: add scroll to top button

--- src/index.js
+++ src/index.js
@@ -1,36 +1,39 @@
-import addImg from './script.js'
+import init from './script.js'
 import './styles/style.scss';
 import './styles/normalize.css';
 
-// addImg();
+// function init() {
+// 	alert('hello')
+// }
+ready(init);
 
-$('#carouselExample').on('slide.bs.carousel', function (e) {
+// $('#carouselExample').on('slide.bs.carousel', function (e) {
 
-	/*
+// 	/*
 
-	CC 2.0 License Iatek LLC 2018
-	Attribution required
+// 	CC 2.0 License Iatek LLC 2018
+// 	Attribution required
   
-	*/
+// 	*/
 
-	let $e = $(e.relatedTarget);
+// 	let $e = $(e.relatedTarget);
 
-	let idx = $e.index();
-	console.log("IDX :  " + idx);
+// 	let idx = $e.index();
+// 	console.log("IDX :  " + idx);
 
-	let itemsPerSlide = 8;
-	let totalItems = $('.carousel-item').length;
+// 	let itemsPerSlide = 8;
+// 	let totalItems = $('.carousel-item').length;
 
-	if (idx >= totalItems - (itemsPerSlide - 1)) {
-		let it = itemsPerSlide - (totalItems - idx);
-		for (let i = 0; i < it; i++) {
-			// append slides to end
-			if (e.direction == "left") {
-				$('.carousel-item').eq(i).appendTo('.carousel-inner');
-			}
-			else {
-				$('.carousel-item').eq(0).appendTo('.carousel-inner');
-			}
-		}
-	}
-});
+// 	if (idx >= totalItems - (itemsPerSlide - 1)) {
+// 		let it = itemsPerSlide - (totalItems - idx);
+// 		for (let i = 0; i < it; i++) {
+// 			// append slides to end
+// 			if (e.direction == "left") {
+// 				$('.carousel-item').eq(i).appendTo('.carousel-inner');
+// 			}
+// 			else {
+// 				$('.carousel-item').eq(0).appendTo('.carousel-inner');
+// 			}
+// 		}
+// 	}
+// });
--- src/script.js
+++ src/script.js
@@ -1,9 +1,34 @@
-import daftcodeImgUrl from './assets/daftcode.png'
-
-export default function() {
-  const section = document.createElement('section')
-  const img = document.createElement('img')
-  img.src = daftcodeImgUrl
-  section.appendChild(img)
-  document.body.appendChild(section)
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
 }
+
+const scrollToTopBtn = document.querySelector(".scroll");
+
+ready(function addScroll() {
+  scrollToTopBtn.addEventListener("click", function() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
+  })
+})
+
+ready(function hideScroll() {
+  window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) 
+    {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }   
+  }
+})
+
+
+
+
