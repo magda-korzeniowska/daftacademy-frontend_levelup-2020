c6be5e0a64791acdf62b1fa3fe06557d082e63bb
magda-korzeniowska <m.korzeniowska@tlen.pl>
2020-04-24 17:33:37 +0200

    refactor: slider

--- src/index.js
+++ src/index.js
@@ -6,34 +6,3 @@ import './styles/normalize.css';
 // 	alert('hello')
 // }
 ready(init);
-
-// $('#carouselExample').on('slide.bs.carousel', function (e) {
-
-// 	/*
-
-// 	CC 2.0 License Iatek LLC 2018
-// 	Attribution required
-  
-// 	*/
-
-// 	let $e = $(e.relatedTarget);
-
-// 	let idx = $e.index();
-// 	console.log("IDX :  " + idx);
-
-// 	let itemsPerSlide = 8;
-// 	let totalItems = $('.carousel-item').length;
-
-// 	if (idx >= totalItems - (itemsPerSlide - 1)) {
-// 		let it = itemsPerSlide - (totalItems - idx);
-// 		for (let i = 0; i < it; i++) {
-// 			// append slides to end
-// 			if (e.direction == "left") {
-// 				$('.carousel-item').eq(i).appendTo('.carousel-inner');
-// 			}
-// 			else {
-// 				$('.carousel-item').eq(0).appendTo('.carousel-inner');
-// 			}
-// 		}
-// 	}
-// });
