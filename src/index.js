51a5d01d58fb462f379b66f843df85a456ba2c2a
magda-korzeniowska <m.korzeniowska@tlen.pl>
2020-05-17 16:44:36 +0200

    feat: fetch data from api in carousel section

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import { addScroll, addYear, showAll, getData } from './script.js'
+import { addScroll, addYear, showAll, fetchData, carousel } from './script.js'
 import './styles/style.scss';
 import './styles/normalize.css';
 
 document.addEventListener('DOMContentLoaded', () => {
+	fetchData()
+		.then(res => {
+			console.log(res);
+			carousel(res);
+		})
+	//  .catch(err => {
+  //     window.alert(`An error occurred while trying to fetch data: ${error}`)
+  //   })
 	addScroll();
 	addYear();
 	showAll();
-	getData();
 });
+
 
--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ const showAll = () => {
     let productsGrid = document.querySelector(".new-arrivals__row");
     for (let i = 0; i < otherProducts; i++) {
       productsGrid.innerHTML +=
-        `
+      `
       <div class="col-6 col-sm-6 col-md-3 col-lg-3 new-arrivals__col">
         <div class="card mb-4 product">
           <img src="https://via.placeholder.com/309x390/d7d7d7/959595?Text=309x390.png" class="card-img-top product__image" alt="product image">
@@ -58,28 +58,95 @@ const showAll = () => {
   })
 }
 
-const getData = () => {
-  let productsData = [];
-  fetch("https://asos2.p.rapidapi.com/products/v2/list?country=US&currency=USD&sort=freshness&lang=en-US&sizeSchema=US&offset=0&categoryId=4209&limit=48&store=US", {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-host": "asos2.p.rapidapi.com",
-      "x-rapidapi-key": "71385867eamshfe05d81dc3bb946p1b2c5cjsne3162cf192c8"
-    }
-  })
-    .then(res =>
-      res.json()
-        .then(data => ({
-          data: data
-        }))
-        .then(res => {
-          productsData.push(res.data.products)
-          console.log(productsData)
-        }))
+// ******* FETCH DATA *******
 
-    .catch(err => {
-      window.alert(`An error occurred while trying to fetch data: ${error}`)
+const fetchData = () => {
+  return new Promise ((resolve, reject) => {
+    
+    fetch("https://asos2.p.rapidapi.com/products/v2/list?country=US&currency=USD&sort=freshness&lang=en-US&sizeSchema=US&offset=0&categoryId=4209&limit=48&store=US", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "asos2.p.rapidapi.com",
+        "x-rapidapi-key": "71385867eamshfe05d81dc3bb946p1b2c5cjsne3162cf192c8"
+      }
     })
+      .then(res => {
+        let productsData = []
+        res.json()
+          .then(data => ({
+            data: data
+          }))
+          .then(res => {
+            res.data.products.forEach(product => {
+              productsData.push({
+                name: product.name,
+                price: product.price.current.value,
+                imageUrl: product.imageUrl,
+                currency: product.price.currency
+              })
+            })
+            resolve(productsData);
+          })
+      })
+      .catch(err => {
+        window.alert(`An error occurred while trying to fetch data: ${error}`)
+      })
+    })  
 }
 
-export { addScroll, addYear, showAll, getData };
+// ******* CAROUSEL *******
+
+const carousel = (products) => {
+
+  let countFirstFour = true;
+  let isCardSet = true;
+  let setCount = 1;
+  let carouselInner = document.querySelector(".carousel-inner");
+
+  for (let i = 1; i < products.length; i++) {
+    if (isCardSet) {
+      insertCarouselItem(countFirstFour, setCount);
+      isCardSet = false
+    }
+    
+    if (i % 4 === 0) {
+      document.getElementById(`slider__row-${setCount}`).innerHTML += insertCards(products[i - 1].imageUrl, products[i - 1].name, products[i - 1].price, products[i - 1].currency)
+      countFirstFour = false; 
+      isCardSet = true;
+      setCount++;    
+    } else {
+      document.getElementById(`slider__row-${setCount}`).innerHTML += (insertCards(products[i - 1].imageUrl, products[i - 1].name, products[i - 1].price, products[i - 1].currency));
+    }
+    
+  }
+
+  function insertCarouselItem(isActive, set) {
+    let active = isActive ? ' active' : '';
+    let result = 
+      `
+      <div id="set-${set}" class="carousel-item ${active}">
+      <div id="slider__row-${set}" class="row slider__row">
+      `;
+
+    carouselInner.innerHTML += result
+  }
+  
+  function insertCards(imageUrl, name, price, currency) {
+
+    let result = `
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 slider__col">
+      <div class="card slider__card">
+        <img src="http://${imageUrl}" class="img-fluid slider__item-image" alt="product image">
+        <div class="card-img-overlay d-flex flex-column justify-content-end">
+          <p class="card-text pb-1 slider__item-description">${name}</h5>
+          <p class="card-text slider__item-price">${price} ${currency}</p>
+        </div> 
+      </div>
+    </div>
+    `;
+      
+    return result;   
+  }
+}
+
+export { addScroll, addYear, showAll, fetchData, carousel };
